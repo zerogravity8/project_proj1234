aaed5dc474fcda8a3f048f5a60e72985594e06c5
zerogravity8 <beliyymishkan@gmail.com>
2023-10-22 17:27:35 +0300

    Merge pull request #27 from DwayneWard/develop
    fix functions

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,33 @@
 from utils import arrs
 
 
+
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+   # Тестирование случая, когда индекс находится в диапазоне массива
+   assert arrs.get([1, 2, 3], 1, "default") == 2
+
+   # Тестирование случая, когда индекс находится вне диапазона массива
+   assert arrs.get([1, 2, 3], 3, "default") == "default"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_my_slice():
+   # Тестирование случая, когда указан только start
+   assert arrs.my_slice([1, 2, 3, 4, 5, 6], start=2) == [3, 4, 5, 6]
+
+   # Тестирование случая, когда указаны start и end
+   assert arrs.my_slice([1, 2, 3, 4, 5, 6], start=1, end=4) == [2, 3, 4]
+
+   # Тестирование случая, когда указан только end
+   assert arrs.my_slice([1, 2, 3, 4, 5, 6], end=3) == [1, 2, 3]
+
+   # Тестирование случая, когда список пустой
+   assert arrs.my_slice([], start=1, end=3) == []
+
+
+
+
+
+
+
+
+
